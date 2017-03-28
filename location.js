77ad98e424c07b7fe952129a53270ac0558c8545
Alex <aleksspeaker@gmail.com>
2017-03-28 18:26:35 +0300

    Messing with location

--- location.js
+++ location.js
@@ -2,6 +2,7 @@
 // prompted by your browser. If you see the error "The Geolocation service
 // failed.", it means you probably did not give permission for the browser to
 // locate you.
+google.maps.event.addListener(map, 'click', find_closest_marker);
 
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -35,4 +36,28 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setContent(browserHasGeolocation ?
                         'Error: The Geolocation service failed.' :
                         'Error: Your browser doesn\'t support geolocation.');
+}
+function rad(x) {return x*Math.PI/180;}
+function find_closest_marker( event ) {
+    var lat = event.latLng.lat();
+    var lng = event.latLng.lng();
+    var R = 6371; // radius of earth in km
+    var distances = [];
+    var closest = -1;
+    for( i=0;i<map.markers.length; i++ ) {
+        var mlat = map.markers[i].position.lat();
+        var mlng = map.markers[i].position.lng();
+        var dLat  = rad(mlat - lat);
+        var dLong = rad(mlng - lng);
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(rad(lat)) * Math.cos(rad(lat)) * Math.sin(dLong/2) * Math.sin(dLong/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c;
+        distances[i] = d;
+        if ( closest == -1 || d < distances[closest] ) {
+            closest = i;
+        }
+    }
+
+    alert(map.markers[closest].title);
 }
